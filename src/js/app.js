55e17e8c7421e231f5c4e00d87b290800a6aef94
MindfulMinun <importantmails4bc@gmail.com>
2017-08-13 18:44:40 -0500

    Let’s see if this works.

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+---
+---
+/*
+ * The following files were concatenated:
+ * auth.js,
+ * account.js,
+ * tchat.js,
+ * dm.js,
+ * a11y.js,
+ * users.js,
+ * inert.min.js,
+**/
+
+//! Start auth.js
+{% include_relative auth.js %}
+//! End auth.js
+//! Start account.js
+{% include_relative account.js %}
+//! End account.js
+//! Start tchat.js
+{% include_relative tchat.js %}
+//! End tchat.js
+//! Start dm.js
+{% include_relative dm.js %}
+//! End dm.js
+//! Start a11y.js
+{% include_relative a11y.js %}
+//! End a11y.js
+//! Start users.js
+{% include_relative users.js %}
+//! End users.js
+//! Start inert.min.js
+{% include_relative inert.min.js %}
+//! End inert.min.js
